caed70f537ab3d434b670cfc60cd148512166944
Sakura Mikano <sakuratmik@gmail.com>
2022-10-09 21:40:10 +0100

    Create models and attributes

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.db.models import Model
+class Thing(Model):
+    name = models.CharField(
+    max_length=30,
+    )
+    description = models.TextField()
+    quantity = models.IntegerField()
 # Create your models here.
