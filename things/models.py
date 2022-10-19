3231b3260f330ab5a26299e49b9fba1121f1497a
Sakura Mikano <sakuratmik@gmail.com>
2022-10-19 14:34:48 +0100

    Add validator for IntegerField

--- things/models.py
+++ things/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 class Thing(models.Model):
     name = models.CharField(
     max_length=30,
@@ -10,6 +12,6 @@ class Thing(models.Model):
     max_length=120,
     )
     quantity = models.IntegerField(
-    
+    validators=[MaxValueValidator(100), MinValueValidator(0)]
     )
 # Create your models here.
